cb58c3488f8b160b873490a0cca9d7942f5b46ba
kiobu <cdolson30@gmail.com>
2020-08-30 01:41:50 -0400

    Remove Clap, it kinda sucks.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
 use spotifyqdbus::*;
-use clap::{Arg, App};
 fn main() {
-    let matches = App::new("Example")
-        .author("kiobu")
-        .about("Mange your Spotify through the terminal.")
-        .arg(Arg::new("play")
-            .long("play")
-            .required(false))
-        .arg(Arg::new("pause")
-            .long("pause")
-            .required(false))
-        .get_matches();
-        
-    if matches.is_present("play") {
-        spotify::exec("Play");
-    } else if matches.is_present("pause") {
-        spotify::exec("Pause");
-    }
+
 }
